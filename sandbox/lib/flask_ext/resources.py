5417b80d8c72d3a2879e975b3cd734ecb9ae03b1
Jill San Luis <jill.devs@gmail.com>
2021-04-03 14:42:04 -0400

    Make member path rule more configurable instead of always relying on id. See #3

--- sandbox/lib/flask_ext/resources.py
+++ sandbox/lib/flask_ext/resources.py
@@ -5,29 +5,29 @@ from dataclasses import dataclass
 class __Action:
     name: str
     http_method: str
-    id_scoped: bool
+    is_member: bool
 
 
-INDEX_ACTION = __Action(name="index", http_method="GET", id_scoped=False)
-SHOW_ACTION = __Action(name="show", http_method="GET", id_scoped=True)
-CREATE_ACTION = __Action(name="create", http_method="POST", id_scoped=False)
-UPDATE_ACTION = __Action(name="update", http_method="PUT", id_scoped=True)
-DELETE_ACTION = __Action(name="delete", http_method="DELETE", id_scoped=True)
+INDEX_ACTION = __Action(name="index", http_method="GET", is_member=False)
+SHOW_ACTION = __Action(name="show", http_method="GET", is_member=True)
+CREATE_ACTION = __Action(name="create", http_method="POST", is_member=False)
+UPDATE_ACTION = __Action(name="update", http_method="PUT", is_member=True)
+DELETE_ACTION = __Action(name="delete", http_method="DELETE", is_member=True)
 
 
 __ALLOWED_ACTIONS = [INDEX_ACTION, SHOW_ACTION, CREATE_ACTION, UPDATE_ACTION, DELETE_ACTION]
 
 
-def register(flask_app, base_uri, resource_module, id_type="int"):
+def register(flask_app, base_uri, resource_module, member_path_rule="<int:id>"):
     for action in __ALLOWED_ACTIONS:
         if hasattr(resource_module, action.name):
-            rule = __rule(base_uri, action, id_type)
+            rule = __rule(base_uri, action, member_path_rule)
             endpoint = f'{base_uri}_{action.name}'.replace('/', '_')
             view_func = getattr(resource_module, action.name)
 
             flask_app.add_url_rule(rule, endpoint, view_func, methods=[action.http_method])
 
 
-def __rule(base_uri, action, id_type):
+def __rule(base_uri, action, member_path_rule):
     rule = f'/{base_uri}'
-    return f'{rule}/<{id_type}:id>' if action.id_scoped else rule
+    return f'{rule}/{member_path_rule}' if action.is_member else rule

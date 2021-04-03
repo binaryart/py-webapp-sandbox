35f213b011d93f9f6653f08ca4ae3e45dfe7ee02
Jill San Luis <jill.devs@gmail.com>
2021-04-03 14:52:15 -0400

    Support nested resources on routes pretty printing. See #3

--- sandbox/lib/flask_ext/resources.py
+++ sandbox/lib/flask_ext/resources.py
@@ -7,6 +7,23 @@ class __Action:
     http_method: str
     is_member: bool
 
+    def rule_for(self, base_uri, member_path_rule):
+        rule = f"/{base_uri}"
+
+        if self.is_member:
+            rule = f"{rule}/{member_path_rule}"
+
+        return rule
+
+    def endpoint_for(self, base_uri):
+        tokens = [
+            t
+            for t in base_uri.split("/")
+            if "<" not in t
+        ]
+
+        return f"{'_'.join(tokens)}_{self.name}"
+
 
 INDEX_ACTION = __Action(name="index", http_method="GET", is_member=False)
 SHOW_ACTION = __Action(name="show", http_method="GET", is_member=True)
@@ -21,13 +38,8 @@ __ALLOWED_ACTIONS = [INDEX_ACTION, SHOW_ACTION, CREATE_ACTION, UPDATE_ACTION, DE
 def register(flask_app, base_uri, resource_module, member_path_rule="<int:id>"):
     for action in __ALLOWED_ACTIONS:
         if hasattr(resource_module, action.name):
-            rule = __rule(base_uri, action, member_path_rule)
-            endpoint = f'{base_uri}_{action.name}'.replace('/', '_')
+            rule = action.rule_for(base_uri, member_path_rule)
+            endpoint = action.endpoint_for(base_uri)
             view_func = getattr(resource_module, action.name)
 
             flask_app.add_url_rule(rule, endpoint, view_func, methods=[action.http_method])
-
-
-def __rule(base_uri, action, member_path_rule):
-    rule = f'/{base_uri}'
-    return f'{rule}/{member_path_rule}' if action.is_member else rule

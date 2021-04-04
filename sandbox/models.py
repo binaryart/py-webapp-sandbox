373ccd58d81b1c9e9f406931ed7b99bcf7051e10
Jill San Luis <jill.devs@gmail.com>
2021-04-03 21:22:36 -0400

    Updated models.py. See #5

--- sandbox/models.py
+++ sandbox/models.py
@@ -1,4 +1,25 @@
 # coding: utf-8
-from sqlalchemy import MetaData
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-metadata = MetaData()
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Album(Base):
+    __tablename__ = 'albums'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    artist = Column(String(255), nullable=False)
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    album_id = Column(ForeignKey('albums.id'), nullable=False)
+
+    album = relationship('Album')

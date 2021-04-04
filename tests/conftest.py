7b65dbc61a2b2a50cd55bfc8bd903ee575406850
Jill San Luis <jill.devs@gmail.com>
2021-04-04 17:11:37 -0400

    Create a database for each tests. See #5

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,37 @@
+import os
+import shutil
+import tempfile
+
 import pytest
 from dotenv import load_dotenv
-
-from sandbox.app import create_app
-
 
 # Load test specific environment variables
 load_dotenv("../.env.test")
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 
 @pytest.fixture
 def client():
+    db_fd, db_path = __new_test_database()
+    os.environ["DATABASE_URL"] = f"sqlite:///{db_path}"
+
+    from sandbox.app import create_app
+
     app = create_app()
     with app.test_client() as client:
         yield client
+
+    try:
+        os.unlink(db_path)
+    finally:
+        os.close(db_fd)
+
+
+def __new_test_database():
+    db_fd, db_path = tempfile.mkstemp()
+
+    template_db = os.path.join(PROJECT_ROOT, os.getenv("DATABASE_FILE"))
+    shutil.copyfile(template_db, db_path)
+
+    return db_fd, db_path
